efbb11efb7f94d2adf94e18d2ab43a510ec4cefe
Artillepsy <artillepsy@gmail.com>
2025-03-26 01:41:07 +0300

    fix(player): fix player rotation not being synchronized

--- Assets/Game/Scripts/Gameplay/Shared/Rotation/PlayerRotation.cs
+++ Assets/Game/Scripts/Gameplay/Shared/Rotation/PlayerRotation.cs
@@ -1,3 +1,4 @@
+using System;
 using Game.Scripts.Gameplay.Shared.Util.Camera;
 using Unity.Netcode;
 using UnityEngine;
@@ -6,6 +7,13 @@ namespace Game.Scripts.Gameplay.Shared.Rotation
 {
 public class PlayerRotation : NetworkBehaviour
 {
+	private Rigidbody _rb;
+	
+	private void Awake()
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
+
 	private void Update()
 	{
 		if (!IsLocalPlayer)
@@ -14,7 +22,8 @@ public class PlayerRotation : NetworkBehaviour
 		}
 
 		var mousePos = CameraHelperC.GetMouseCursorWorldPos();
-		transform.LookAt(mousePos, Vector3.up);
+		_rb.rotation = Quaternion.LookRotation(mousePos - _rb.position, Vector3.up);
+		//transform.LookAt(mousePos, Vector3.up);
 		
 		Debug.DrawRay(mousePos, Vector3.up * 3, Color.red);
 	}
